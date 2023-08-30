a360a90c298ee76df8628491c0c328be87a45b5a
Samuel Gipit <samuelgipit@gmail.com>
2023-08-30 12:25:16 +0800

    added guess number game

--- guessNumber.py
+++ guessNumber.py
@@ -1,12 +1,16 @@
 from random import randint
 
-number = randint(0, 1)
-print(f"answer is: {number}")
-tries = 3
-while tries > 0:
-    guess = int(input("Enter number: "))
-    if number == guess:
-        print("You won")
-        break
-    else:
-        tries -= 1
+class GuessNumberGame:
+    """A game where player guess the number chosen by computer"""
+    def __init__(self):
+        self.number = randint(0,10)
+        self.guess = None
+        
+    def play_game(self):
+        while not self.guess == self.number:
+            self.guess = int(input("enter number: "))
+
+        print("you won")
+
+game = GuessNumberGame()
+game.play_game()
